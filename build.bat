122814ba53c4b22fac81affac7a747a2eb21e1d5
Dmitry Veresov <dveresov@1stquad.com>
2017-09-13 12:25:55 +0200

    + Update build process;

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
+ant minify-all -f D:\Git\ckeditor-track-changes.git\build\build-lite.ant
 ant webpack-lite -f D:\Git\ckeditor-track-changes.git\build\build-lite.ant
+ant make-dist -f D:\Git\ckeditor-track-changes.git\build\build-lite.ant
 pause
